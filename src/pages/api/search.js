7bc13bb01ae233856732d93727de73f8ebf0729d
Seniru <seniruudugama25@gmail.com>
2022-07-22 10:30:11 +0530

    implementing book search functionality

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -5,10 +5,17 @@ export default async function searchQuery(req, res) {
     res.status(405).json({ message: "Method is not accepted" });
   } else if (req.method === "POST") {
     let search = req.body.search;
-    const {data} = await axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${process.env.GOOGLE_API_KEY}&maxResults=10`
-      )
-     res.json({ message: data.items });
+    const { data } = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${search.toLowerCase()}&key=${process.env.GOOGLE_API_KEY}&maxResults=20`
+    );
+
+    res.status(200).json({
+      message: data.items.map(book => {
+        return {
+          imageLink: book.volumeInfo.imageLinks.thumbnail,
+          title: book.volumeInfo.title,
+        };
+      }),
+    });
   }
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,14 +3,37 @@ import prisma from "../lib/prisma";
 import Logo from "../components/Logo";
 import { HiOutlineSearch } from "react-icons/hi";
 import axios from "axios";
+import { useState } from "react";
+import { DebounceInput } from "react-debounce-input";
 
 export default function dashboard({ user }) {
-  const handleSubmit = async e => {
-    console.log(e.target.value);
-   const {data} = await axios.post("/api/search", {
-      search: e.target.value,
-    });
-    console.log(data);
+  const [books, setBooks] = useState([]);
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [isSearchQuery, setIsSearchQuery] = useState(false);
+  const handleChange = async e => {
+    if (e.target.value.length === 0) {
+      setIsSearchQuery(false);
+    } else if (e.target.value.length > 0) {
+      setIsSearchQuery(true);
+      console.log(e.target.value);
+      const { data } = await axios.post("/api/search", {
+        search: e.target.value,
+      });
+      if (data) {
+        setBooks(data.message);
+      }
+
+      // data.message.map(book => {
+      //   console.log(book.title);
+      //   return (
+      //     <div>
+      //       <p className="text-white text-lg">{book.title}</p>
+      //     </div>
+      //   );
+      // });
+
+      // console.log(data);
+    }
   };
 
   return (
@@ -25,21 +48,39 @@ export default function dashboard({ user }) {
         </span>{" "}
         ! Welcome to your dashboard.
       </h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center flex-col ">
         <form
           action="/dashboard"
           method="POST"
           className="relative flex mt-8 justify-center items-center w-[35rem] "
-          onChange={handleSubmit}
         >
           <HiOutlineSearch className="text-zinc-400 text-2xl left-[34rem] " />
-          <input
-            className=" w-[90%] ml-[-2.8rem] inline-block  pl-14 py-4 rounded-lg  placeholder:text-lg  bg-[rgba(255,255,255,.2)] focus:outline-none focus:caret-[#2CD261] text-white  focus:w-[100%] transition-all duration-200 ease-in-out"
+
+          <DebounceInput
+            className=" w-[90%] ml-[-2.8rem] inline-block  pl-14 py-4 rounded-lg  placeholder:text-lg  bg-[rgba(255,255,255,.2)] focus:outline-none focus:caret-[#2CD261] text-white  focus:w-[100%] transition-all duration-200 ease-in-out "
             type="text"
+            debounceTimeout={500}
+            onChange={handleChange}
             placeholder="Search For Books..."
             autoComplete="off"
+            onFocus={() => setIsSearchFocused(true)}
+            onBlur={() => {
+              setIsSearchFocused(false);
+            }}
           />
         </form>
+        {isSearchFocused && isSearchQuery && (
+          <ul className="dropdown h-auto max-h-64 w-[35rem] mt-4 rounded-lg mr-[1.27rem] bg-[rgba(255,255,255,.2)] overflow-y-scroll">
+            {books.map(book => (
+              <li
+                key={books.indexOf(book)}
+                className="text-white text-lg p-2 border-b-[1px] border-gray-700 first:pt-2 last:pb-2 "
+              >
+                {book.title}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
