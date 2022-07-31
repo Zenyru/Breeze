42bcc0c5a1592d8f6fc75935e09f220660eab983
Seniru <seniruudugama25@gmail.com>
2022-07-31 21:05:15 +0530

    modifying the responsiveness

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -52,12 +52,17 @@ export default function BookContainer({ settingFiltered, info }) {
   };
 
   return (
-    <div className="text-white max-w-7xl mx-auto h-full flex justify-center relative z-0 pb-4 xxs:pb-0  lg:pb-8 xs:min-h-screen">
+    <div className="text-white max-w-7xl mx-auto h-full flex justify-center relative z-0 pb-4 xs:pb-0  lg:pb-8 xs:min-h-screen">
       <div className="h-48 w-48 rounded-xl blur-3xl animate-bounce-slow 2xl:hidden absolute left-[-2rem] bottom-4 bg-[rgba(166,235,18,0.24)] mb-10"></div>
       <div className="h-48 w-48 rounded-xl blur-3xl animate-bounce-slow 2xl:hidden absolute right-[-2rem] top-[5rem] bg-[rgba(44,210,97,0.24)]"></div>
-      <div className=" w-[50rem] h-[30rem] md:w-[40rem] sm:w-[30rem] xxs:h-[35rem]  md:h-[26.5rem] bg-[#ffffff36] mt-12 rounded-lg ">
-        <ul className={styles.bookContainerScroll}>
-          {/* mapping the book array in to list items */}
+      <div
+        className=" w-[50rem] md:w-[40rem] sm:w-[30rem] xs:!min-h-[80vh] bg-[#ffffff36] mt-12 rounded-lg h-full"
+        style={{
+          minHeight: "calc(100vh - 280px)",
+        }}
+      >
+        {/* mapping the book array in to list items */}
+        <div className={`${styles.bookContainerScroll} xs:!min-h-[80vh]`}>
           {books?.map((book, index) => {
             const pageCount = book.pageCount.toString();
             const updatedBook = updatedBooks.find(item => item.id == book.id);
@@ -77,7 +82,7 @@ export default function BookContainer({ settingFiltered, info }) {
               />
             );
           })}
-        </ul>
+        </div>
       </div>
     </div>
   );
